c817c7434267831287260c4cf21bfdd914fe9fbb
mfavorito1995 <mfavorito1995@gmail.com>
2024-04-28 20:58:53 -0700

    Starting style rewrite

--- mf_site/blog/urls.py
+++ mf_site/blog/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 app_name="blog"
 urlpatterns = [
-    path("", views.index, name='gallery'),
+    path("", views.index, name='index'),
     path("<int:blog_id>/", views.a_blog, name="a_blog")
 ]
